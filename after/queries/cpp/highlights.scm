faadf8be52078ad21bf7817ab61f35fd5e0908d6
NeKon69 <nobodqwe@gmail.com>
2026-03-01 21:51:12 +0300

    [fix] add normal gitignore and delete some crap

--- after/queries/cpp/highlights.scm
+++
@@ -1,7 +0,0 @@
-;; extends
-(alignas_qualifier) @error
-(function_definition
-  type: (type_identifier) @constant.macro
-  (ERROR
-    (identifier) @type.builtin
-    (identifier) @keyword.function))
